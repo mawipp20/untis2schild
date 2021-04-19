508ba6f66c5fd5543aca21e2d5db89fea9815450
Martin <wippersteg@zweiundzwei.de>
2021-04-19 12:32:18 +0200

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-param" content="_csrf">
+    <meta name="csrf-token" content="EQqgD62krfOLqO6Ilfljha6-hBIhmt543Qz5pa4YQXpzee5K487hufjnlN_h1AnE34zPfkS3ugGoVZz23iEPFw==">
+    <title>untis2schild</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <link href="untis2schild.css?v=1600667029" rel="stylesheet">
+<body>
+<div class="container">
+
+<p>Test</p>
+
+
+
+
+</div>
+</body>
+</html>
